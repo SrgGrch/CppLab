7c009cc3c49a3d0b8a57810342c6d116caec15d6
Sergey Grechishnikov <Srg.Grch@gmail.com>
2018-10-12 13:47:35 +0700

    main improvedgit

--- cppprog/Date.cpp
+++ cppprog/Date.cpp
@@ -6,8 +6,9 @@
 int main() {
 	Date d1 = Date(28, "Пятница",9, 2018);
 	Date d2 = Date(20, "Пятница", 2, 2018);
-	Date d3;
-	d3 = d2.comp(d1, d2);
-	d3.setMounth(4);
+	d2.setDay(30);
+	if (d2.getMounth == 2) d2.setDay(9);
+	Date d3 = d2.comp(d1, d2);
+	int days = d2.dateToDays();
 	_getch();
 } 
