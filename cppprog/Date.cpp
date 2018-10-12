f247f361b1e8629a884b2dc1adba7817952b647a
Sergey Grechishnikov <Srg.Grch@gmail.com>
2018-10-12 14:41:51 +0700

    minor chages

--- cppprog/Date.cpp
+++ cppprog/Date.cpp
@@ -1,14 +1,15 @@
-#include "Header.h"
-#include <iostream>
-#include <conio.h>
+	#include "Header.h"
+	#include <iostream>
+	#include <conio.h>
 
 
-int main() {
-	Date d1 = Date(28, "Пятница",9, 2018);
-	Date d2 = Date(20, "Пятница", 2, 2018);
-	d2.setDay(30);
-	if (d2.getMounth == 2) d2.setDay(9);
-	Date d3 = d2.comp(d1, d2);
-	int days = d2.dateToDays();
-	_getch();
-} 
+	int main() {
+		Date d1 = Date(28, "Пятница",9, 2018);
+		Date d2 = Date(20, "Пятница", 2, 2018);
+		d2.setDay(30);
+		if (d2.getMounth() == 2) d2.setDay(9);
+		Date d3 = d2.comp(d1, d2);
+		int days = d2.dateToDays();
+		std::cout << d3.getDay() << '.' << d3.getMounth() << '.' << d3.getYear();
+		_getch();
+	} 
